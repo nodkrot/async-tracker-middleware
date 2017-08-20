5dabd18ae947ce8c27aeb738957ffbcbe20ca965
Vyacheslav Alayev <valayev1@gmail.com>
2017-08-19 20:05:46 -0400

    Add code

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Redux middleware for async action state tracking
+ * @param  {Object} options
+ *                  options.actionType - action type that will be dispatched
+ *                  options.actionPrefixes - custom async action prefixes
+ * @return {Object}
+ */
+export function asyncStateTracker(options = {}) {
+    return () => (next) => (action) => {
+        const ASYNC_STATE_TRACKER = options.actionType || 'ASYNC_STATE_TRACKER';
+        const actionSuffixes = options.actionSuffixes || ['_PENDING', '_FULFILLED', '_REJECTED'];
+        const prefixMatch = action.type.match(new RegExp(`${actionSuffixes.join('|')}$`));
+
+        if (prefixMatch) {
+            const prefix = prefixMatch[0];
+            const actionKey = action.type.slice(0, -prefix.length);
+            const actionValue = prefix === actionSuffixes[0];
+
+            next({
+                type: ASYNC_STATE_TRACKER,
+                payload: { [actionKey]: actionValue }
+            });
+        }
+
+        return next(action);
+    };
+}
+
+export default asyncStateTracker();
